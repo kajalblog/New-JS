4290979bcc7f92d8162771f4447b923f6bab1f9c
kajalblog <kajalparchandee.com>
2024-12-22 10:21:34 +0530

    JS-Datatypes

---
+++ day-3/dataTypes.js
@@ -0,0 +1,25 @@
+// 7 Primitive & Non-primitive(object) datatypes
+// nn bb ss u
+// number,null,boolean,bigInt,String,Symbol,Undefined
+
+let a = 12;
+let b = null;
+let c = true;
+let d = BigInt('450');
+let e = "Kavita";
+let f = Symbol("I am web Developer");
+let g;
+
+console.log(a, b, c, d, f, g);
+
+// Object (Non-Primitive)
+
+const item = {
+    "Ken": true,
+    "Riyan": false,
+    "Sita": 34,
+    "Siya": "Welcome",
+    "Riya": null,
+
+};
+console.log(item["Sita"]);
