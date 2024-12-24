723e15ef6d14e8148ce4a2b21cf15e2ed65c559e
kajalblog <kajalparchandee.com>
2024-12-24 09:20:40 +0530

    Conditional operators pratice

---
+++ day-6-(ConditionaOperators)/pratice.js
@@ -0,0 +1,20 @@
+let n = 12;
+if (n % 2 == 0 && n % 3 == 0) {
+    console.log("Num is divisible by 2 & 3");
+}
+else {
+    console.log("Num is not divisible by 2 & 3");
+
+}
+n = 10;
+if (n % 2 == 0 || n % 3 == 0) {
+    console.log("Num is divisible by 2");
+}
+else {
+    console.log("Num is not divisible by  3");
+
+}
+
+let age = 20;
+let a = age > 18 ? 'You can drive' : 'Yo can not drive';
+console.log(a);
