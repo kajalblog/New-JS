eaeb41737f6e20a4aa3756563e31dd63e4440444
kajalblog <kajalparchandee.com>
2024-12-26 12:19:57 +0530

    Template Literal

---
+++ day-10(template-Liiteral)/template_Literal.js
@@ -0,0 +1,26 @@
+let a = 29;
+let b = 74;
+console.log(`Value of a is ${a} `);
+console.log(`Value of b is ${b} `);
+
+// Escap sequence character\
+// For single quotes:  \’  (backslash followed by single quote)
+// For double quotes: \”  (backslash followed by double quotes)
+// \n
+// \t
+// \r
+// let fruit="bana\'na";
+let fruit = "Hello \'friends\' Welcome to this channel";
+
+console.log(fruit);
+
+let msg = "Hello friends \nWelcome to this channel";
+console.log(msg);
+let msg1 = "Hello friends \tWelcome to this channel";
+console.log(msg1);
+let msg2 = "Lorem Ipsum is simply dummy \rtext of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"
+
+
+
+
+console.log(msg2);
