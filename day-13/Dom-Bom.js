0b9ea55f606cf433eb63479979cd01c1958e3e66
kajalblog <kajalparchandee.com>
2025-01-03 08:43:11 +0530

    Dom & Bom

---
+++ day-13/Dom-Bom.js
@@ -0,0 +1,19 @@
+console.log(console);
+console.warn("Test");
+console.info("Info Console");
+let a = 5;
+if (a < 10) {
+    console.assert(a < 10)
+    console.warn("True");
+}
+else {
+    console.warn("False");
+
+}
+
+// console.log(window)
+window.console.log(window);
+window.console.log(window);
+console.log(document);
+document.write("Hello")
+document.body.style.background = "red"
