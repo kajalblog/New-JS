6528e265fe9a88c396aa8c8864cf2a241fc53e9b
kajalblog <kajalparchandee.com>
2025-01-04 15:35:29 +0530

    Exercise2

---
+++ day-14/Exercise2.js
@@ -0,0 +1,17 @@
+// 1 Write a program to change url if the user enter number greater than 4
+
+
+// const cannDrive = (num) => {
+//     return num > 18 ? true : false;
+// }
+// let num = prompt("Enter Your num");
+// num = +num;
+
+// if(num>4)
+// {
+//     location.href="https://google.com"
+// }
+
+let color1 = prompt("Enter a color");
+document.body.style.background = color1;
+
