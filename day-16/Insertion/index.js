61c44cacca412a1ea4cd6cff6dde4a2ce9fb62ca
kajalblog <kajalparchandee.com>
2025-01-16 09:16:15 +0530

    Create Element

---
+++ day-16/Insertion/index.js
@@ -0,0 +1,11 @@
+let b = document.getElementsByTagName('div')[0];
+b.innerHTML = b.innerHTML + "<h1>This is js series..!</h1>";
+
+let div = document.createElement("div");
+div.innerHTML = "<h1>This is created element</h1>";
+// b.appendChild(div);
+// b.append(div);
+// b.prepend(div);
+// b.after(div);
+// b.after(div);
+// b.replaceWith(div)
